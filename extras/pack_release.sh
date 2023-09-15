00e43498be37aa954c68399c6c8ac88ffdab319a
MATSUOKA Takashi <matsujirushi@live.jp>
2023-09-15 19:57:34 +0900

    Merge 1.feat:support for wio_tracker_1110 board

--- extras/pack_release.sh
+++ extras/pack_release.sh
@@ -30,6 +30,3 @@ tar --transform "s|$FOLDERNAME|$VERSION|g"  --exclude=extras/** --exclude=.git* 
 cd -
 
 mv ../$FOLDERNAME-$VERSION.tar.bz2 .
-
-echo checksum ... SHA-256:`sha256sum $FOLDERNAME-$VERSION.tar.bz2 | awk '{print $1}'`
-echo size     ... `wc -c Seeed_nRF52_Boards-1.1.4.tar.bz2 | awk '{print $1}'`
